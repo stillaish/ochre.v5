1d42afb70116b44e3b010b714bda3961328a5789
stillaish <bossbm05@gmail.com>
2025-09-10 11:36:59 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: {
-        main: './index.html'
-      }
+      input: './index.html'
     }
   },
   optimizeDeps: {
