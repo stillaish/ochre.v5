eea44c1ef6ac61e30eb71d899f1001da8194912b
stillaish <bossbm05@gmail.com>
2025-09-10 12:10:50 +0530

    modified:   client/tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          50: '#eff6ff',
+          50: '#eff6',
           100: '#dbeafe',
           200: '#bfdbfe',
           300: '#93c5fd',
